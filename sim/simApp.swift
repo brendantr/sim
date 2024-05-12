d56857d36566d6483eacb295abcf87068396858a
Brendan Rodriguez <brendantr57@aol.com>
2024-05-11 23:14:40 -0400

    Initial Commit

---
+++ sim/simApp.swift
@@ -0,0 +1,17 @@
+//
+//  simApp.swift
+//  sim
+//
+//  Created by Brendan  Rodriguez on 5/11/24.
+//
+
+import SwiftUI
+
+@main
+struct simApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
